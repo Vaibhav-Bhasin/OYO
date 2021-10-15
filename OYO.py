67b86564c4462399c46480893eefbac78cb841e6
Vaibhav Bhasin <75664061+Vaibhav-Bhasin@users.noreply.github.com>
2021-10-15 22:10:33 +0530

    Update OYO.py

--- OYO.py
+++ OYO.py
@@ -1,1 +1,24 @@
 
+
+def car_cost(days):
+  if days<3:
+    return 5000*days
+  elif 3<=days<7:
+    return 5000*days-200
+  elif days>=7:
+    return 5000*days-500
+days = int(input("How many days: "))
+print("Your car rent for ",days,"days is",car_cost(days))
+
+
+def hotel_cost(nights):
+    return nights*5000
+#cost: 5000/night
+nights = int(input("How Many Nights You Want To Stay:: \n"))
+print("Your Hotel cost is",hotel_cost(nights))
+
+
+def hotel_room_cost(room):
+  if room=="Single":
+    return 1500
+
